db751387645fd2ceb39270eb14d065ffae7a66f7
Mads Franzen <madsfranzen@live.dk>
2025-09-20 21:37:21 +0200

    feat: add clear(x) button to searchbar

--- src/framework/components/SearchBar.tsx
+++ src/framework/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Button } from './Button';
 
 interface SearchBarProps {
 	value: string;
@@ -7,8 +8,20 @@ interface SearchBarProps {
 	placeholder: string;
 }
 
+export const SearchBar = ({ value, onChange, placeholder }: SearchBarProps) => (
+	<SearchBarWrapper>
+		<StyledSearchBar
+			type="text"
+			placeholder={placeholder}
+			value={value}
+			onChange={e => onChange(e.target.value)}
+		/>
+		{value && <ClearButton onClick={() => onChange('')}>x</ClearButton>}
+	</SearchBarWrapper>
+);
+
 const StyledSearchBar = styled.input`
-  padding: 8px 12px;
+  padding: 8px 32px 8px 12px; /* extra right padding to not overlap clear button */
   border-radius: 8px;
   border: 1px solid ${({ theme }) => theme.surfaceStroke};
   width: 100%;
@@ -21,11 +34,22 @@ const StyledSearchBar = styled.input`
   }
 `;
 
-export const SearchBar = ({ value, onChange, placeholder }: SearchBarProps) => (
-	<StyledSearchBar
-		type="text"
-		placeholder={placeholder}
-		value={value}
-		onChange={e => onChange(e.target.value)}
-	/>
-);
+const SearchBarWrapper = styled.div`
+  align-items: center;
+  gap: 8px;
+`;
+ 
+const ClearButton = styled(Button)(
+	({ theme }) => css`
+    background-color: transparent;
+    color: ${theme.fade6};
+    transform: translateX(-110%);
+    transition:
+      background-color 150ms linear,
+      color 150ms linear,
+      transform 150ms linear;
+
+    &:hover, &:focus-visible {
+	  color: ${theme.text};
+    }
+`);
